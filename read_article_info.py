8942206330e181ecbd0fb87f148d480ce9ab85ea
Jong Yoon Lee <yoonlee95@gmail.com>
2018-09-15 11:11:25 -0500

    fixed bugs and made it work with ssh

--- read_article_info.py
+++ read_article_info.py
@@ -10,12 +10,24 @@ def download_file(path, download_url):
 
     download_dir = path
     options = webdriver.ChromeOptions()
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+
+
+    # options.add_argument('--headless')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-setuid-sandbox')
+    #options.add_argument('--disable-dev-shm-usage')
+    #options.add_argument('--ignore-certificate-errors')
+    # profile = {"plugins.always_open_pdf_externally": True}
     profile = {"plugins.plugins_list": [{"enabled": False, "name": "Chrome PDF Viewer"}], # Disable Chrome's PDF Viewer
                "download.default_directory": download_dir , "download.extensions_to_open": "applications/pdf"}  
 
+    print download_dir
     options.add_experimental_option("prefs", profile)
     driver = webdriver.Chrome(chrome_options=options)  # Optional argument, if not specified will search path.
     driver.get("https://dl.acm.org/"+download_url)
+
     for file in os.listdir(path):
         if file.endswith(".pdf"):
             #print file
@@ -31,7 +43,9 @@ def download_file(path, download_url):
 
 def init_webdriver():
   options = webdriver.ChromeOptions()
-  options.add_argument('headless')
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--ignore-certificate-errors')
 
   driver = webdriver.Chrome(chrome_options=options)
@@ -41,22 +55,39 @@ def get_abstract(soup):
     l = soup.find_all("div", style="display:inline")
     #print l[0].text
     #abstract_text = soup.find("div", id="abstract")
-    abstract_text =  l[0].text
-    if abstract_text is not None:
-        return abstract_text
-        #return abstract_text.getText().e
-    else:
-        print "failed to extract abstract ---->", soup.find("div", id="divmain").find("div").find("h1")
+    try:
+        abstract_text =  l[0].text
+        if abstract_text is not None:
+            return abstract_text.strip()
+            #return abstract_text.getText().e
+        else:
+            print "failed to extract abstract ---->", soup.find("div", id="divmain").find("div").find("h1")
+            return ""
+    except:
         return ""
 def get_bibilometers(soup):
     bibilometers = {}
 
     bibilometer_box = soup.find("div", id="divmain").find("td", class_="small-text")
     bibilometer_lines = bibilometer_box.getText().split("\n")
-    bibilometers["CitationCount"] = int(bibilometer_lines[1].split(":")[1].replace(',',''))
-    bibilometers["Downloads_cumulative"] = int(bibilometer_lines[2].split(":")[1].replace(',',''))
-    bibilometers["Downloads_12Months"] = int(bibilometer_lines[3].split(":")[1].replace(',',''))
-    bibilometers["Downloads_6Weeks"] = int(bibilometer_lines[4].split(":")[1].replace(',',''))
+    bibilometer_lines = [x for x in bibilometer_lines if len(x) > 3]
+   
+    try: 
+        bibilometers["CitationCount"] = int(bibilometer_lines[0].split(":")[1].replace(',',''))
+    except:
+        bibilometers["CitationCount"] = 0
+    try: 
+    	bibilometers["Downloads_cumulative"] = int(bibilometer_lines[1].split(":")[1].replace(',',''))
+    except:
+    	bibilometers["Downloads_cumulative"] = 0
+    try:
+        bibilometers["Downloads_12Months"] = int(bibilometer_lines[2].split(":")[1].replace(',',''))
+    except:
+        bibilometers["Downloads_12Months"] = 0
+    try:
+        bibilometers["Downloads_6Weeks"] = int(bibilometer_lines[3].split(":")[1].replace(',',''))
+    except:
+        bibilometers["Downloads_6Weeks"] = 0
 
     return bibilometers
 
@@ -67,7 +98,11 @@ def get_authors(soup):
     for author_tag in author_names:
         author = {}
         author["Name"] = author_tag.string.strip()
-        author["Affiliation"] = author_tag.parent.next_sibling.next_sibling.find("small").string.strip()
+	
+	try:
+            author["Affiliation"] = author_tag.parent.next_sibling.next_sibling.find("small").string.strip()
+	except:
+	    author["Affiliation"] = ""
         authors.append(author)
 
     return authors
@@ -123,7 +158,7 @@ def get_published_year(soup):
 
 def get_title(soup):
     title_tag = soup.find("div", id="divmain").find("div").find("h1").text.encode('utf-8')
-    print soup.find("div", id="divmain").find("div").find("h1")
+    print soup.find("div", id="divmain").find("div").find("h1").text.encode('utf-8')
     if title_tag is not None:
         #return title_tag.string.strip()
         return title_tag
@@ -135,10 +170,10 @@ def get_pdf_link(soup):
         #print links[0]['href']
         return links[0]['href']
     return None
-def get_article_detail(driver, html):
+def get_article_detail(driver, html, output):
   detail = {}
 
-  #print "https://dl.acm.org/"+html+"&preflayout=flat"
+  print "https://dl.acm.org/"+html+"&preflayout=flat"
   driver.get("https://dl.acm.org/"+html+"&preflayout=flat")
   driver.implicitly_wait(1)
   soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -155,17 +190,19 @@ def get_article_detail(driver, html):
 
   folder_name = html.split("cfm?id=")[1].strip()
 
+
+  if not os.path.exists(output+folder_name):
+      os.makedirs(output+folder_name)
+  with open(output+folder_name+'/detail.json', 'w') as outfile:
+      json.dump(detail, outfile, indent=4)
+
+  # download pdf
   link =get_pdf_link(soup)
   if link != None:
     download_file("output/"+folder_name, link)
 
-  if not os.path.exists("output/"+folder_name):
-      os.makedirs("output/"+folder_name)
-  with open("output/"+folder_name+'/detail.json', 'w') as outfile:
-      json.dump(detail, outfile, indent=4)
-
 article_list = {}
-article_list_file_name = 'micro2016-17-test.json'
+article_list_file_name = 'isca.json'
 with open(article_list_file_name, 'r') as f:
     article_list = json.load(f)
 
@@ -173,7 +210,6 @@ Driver = init_webdriver()
 
 for conf, articles in article_list.iteritems():
     print conf
+    
     for article in articles:
-        get_article_detail(Driver, article)
-        #break
-#break
+        get_article_detail(Driver, article, "isca/")
